4e1bc0ae0d5ce22af1cb01a5fcbea54e541b366e
ardauslu <ardauslu0@gmail.com>
2025-04-21 16:18:24 +0300

    fasfasfas

--- src/Login.js
+++ src/Login.js
@@ -22,6 +22,7 @@ const Login = () => {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
           },
           body: JSON.stringify({ username, password }),
         });

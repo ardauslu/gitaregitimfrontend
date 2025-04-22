0fef6b91fcb495db1cc977264f71e24f4cdb5c24
ardauslu <ardauslu0@gmail.com>
2025-04-22 22:57:56 +0300

    =searchterm

--- src/Home.js
+++ src/Home.js
@@ -32,11 +32,11 @@ const Home = () => {
     }
 
     try {
-      const response = await fetch(`https://redditbackend4.onrender.com/api/reddit/fetch/${searchTerm}`);
+      const response = await fetch(`https://redditbackend4.onrender.com/api/reddit/fetch/?subreddit=${searchTerm}`);
       if (response.ok) {
         const data = await response.json();
         if (data.message === "Veriler MongoDB'ye kaydedildi!") {
-          const postsResponse = await fetch(`https://redditbackend4.onrender.com/api/reddit/posts/${searchTerm}`);
+          const postsResponse = await fetch(`https://redditbackend4.onrender.com/api/reddit/posts/?subreddit=${searchTerm}`);
           if (postsResponse.ok) {
             const postsData = await postsResponse.json();
             setPosts(postsData || []);
@@ -62,7 +62,7 @@ const Home = () => {
     setSelectedSubreddit(subreddit);
     setCurrentPage(1);
     try {
-      const response = await fetch(`https://redditbackend4.onrender.com/api/reddit/posts/${subreddit}`);
+      const response = await fetch(`https://redditbackend4.onrender.com/api/reddit/posts/?subreddit=${searchTerm}`);
       if (response.ok) {
         const data = await response.json();
         if (Array.isArray(data)) {
@@ -82,7 +82,7 @@ const Home = () => {
   };
   const handleDeleteSubreddit = async (subreddit) => {
     try {
-      const response = await fetch(`https://redditbackend4.onrender.com/api/reddit/posts/${subreddit}`, {
+      const response = await fetch(`https://redditbackend4.onrender.com/api/reddit/posts/?subreddit=${searchTerm}`, {
         method: "DELETE",
       });
       if (response.ok) {
